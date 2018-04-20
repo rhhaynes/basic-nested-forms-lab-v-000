b4ba2c3cf2ba440a5a9d964b3134dd90c372a422
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-20 17:31:50 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,5 @@
+require 'pry'
 class RecipesController < ApplicationController
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
 
   def index
     @recipes = Recipe.all
@@ -9,8 +7,25 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    binding.pry
+    @recipe = Recipe.new(recipe_params)
+    if !!@recipe.save then redirect_to recipe_path(@recipe)
+    else render :new
+    end
+  end
+  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  private
+  
+  def recipe_params
+    params.require(:recipe).permit(:title, :ingredients => [:name, :quantity])
   end
 end
